bc04bec95e019c8c5d5203827db60667021178b8
sfoozi <156290548+sfoozi@users.noreply.github.com>
2025-09-23 12:32:39 +0500

    Update Prem1.lua

--- Prem1.lua
+++ Prem1.lua
@@ -1,4 +1,4 @@
-local ALLOWED_USERS = {"JellyZer02021,JacksonStreamLava,Turb0N30nMax2005,Rift_Void73,Xx_RocketCircuitCode,alihan12345aaa,DEATH_LOH,Player12345,User,User,User,User"}
+local ALLOWED_USERS = {"JellyZer02021", "JacksonStreamLava", "Turb0N30nMax2005", "Rift_Void73", "Xx_RocketCircuitCode", "alihan12345aaa", "DEATH_LOH", "Player", "Player", "Player", "Player", "Player", "Player", "Player", "Player"}
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 if not LocalPlayer then
